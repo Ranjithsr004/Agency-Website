4f8c4974a15546d21d4b3df58dfd3a80edfe0d66
Ranjith <ranjithunjalur@gmail.com>
2023-01-31 20:25:26 +0530

    hi

---
+++ main.js
@@ -0,0 +1,12 @@
+function menuClick()
+{
+    var menu=document.getElementById('mobilemenu');
+    if(menu.classList.contains('hidden'))
+    {
+        menu.classList.remove('hidden');
+    }
+    else
+    {
+        menu.classList.add('hidden');
+    }
+}
